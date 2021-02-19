a1a02852cb8c92590f071da08a824cbc2528e610
starnata <starnata_k@yahoo.com>
2021-02-18 23:39:14 -0500

    routing

---
+++ Assignment_2/Controllers/J1Controller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment_2.Controllers
+{
+    public class J1Controller : ApiController
+    {
+
+
+        /// <summary>
+        /// jgaipjgpoajgpoaj
+        /// </summary>
+        [HttpGet]
+        [Route("api/J1/Menu/{burger}/{side}/{drink}/{dessert}")]
+        public string Menu()
+        {
+            return "Hello World";
+        }
+
+
+
+           
+
+
+    }
+}
